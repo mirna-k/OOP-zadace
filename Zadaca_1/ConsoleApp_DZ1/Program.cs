e48f8fb8a310b6602c0145720c89c589f1112d61
Mirna-K <mirna_krpic@hotmail.com>
2021-11-22 23:05:44 +0100

    "my first commit message"

---
+++ Zadaca_1/ConsoleApp_DZ1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary_DZ1.Weather.RunDemoForHW1();
+        }
+    }
+}
