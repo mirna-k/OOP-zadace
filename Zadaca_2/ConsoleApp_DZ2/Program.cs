b05dbc5ee37170627f059ffc645544cb8759f0f3
Mirna-K <mirna_krpic@hotmail.com>
2021-11-29 14:56:44 +0100

    Zadaca_2

---
+++ Zadaca_2/ConsoleApp_DZ2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_DZ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary_DZ2.ForecastUtilities.RunDemoForHW2();
+        }
+    }
+}
